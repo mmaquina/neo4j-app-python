3bb48e1654e664741ed9c2ce52692657932a7f46
mmaquina <mmaquina@gmail.com>
2024-11-01 17:38:21 -0300

    can connect to neo4j driver

--- api/neo4j.py
+++ api/neo4j.py
@@ -9,9 +9,9 @@ Initiate the Neo4j Driver
 """
 # tag::initDriver[]
 def init_driver(uri, username, password):
-    # TODO: Create an instance of the driver here
-    current_app.driver = None
-
+    current_app.driver = GraphDatabase.driver(uri, auth=(username, password))
+    if current_app.driver.verify_connectivity():
+        return current_app.driver
     return None
 # end::initDriver[]
 
